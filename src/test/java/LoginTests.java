cdb6e3981265c8a4e6c71608be26bcb44ed6f412
dariaproit <dariad.pro.it@gmail.com>
2023-08-25 18:27:06 -0700

    Login tests updated

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,8 +8,8 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
-    @Test (description = "Login with empty email and password")
-    public void LoginEmptyEmailPassword() {
+    @Test (description = "Successful Login")
+    public void SuccessfulLogin() {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -25,14 +25,19 @@ public class LoginTests extends BaseTest {
         WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
         emailInput.click();
         emailInput.clear();
+        emailInput.sendKeys("demo@class.com");
+
         WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
         passwordInput.click();
         passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent");
+
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
 
-        Assert.assertEquals(driver.getCurrentUrl(), loginUrl, "Wrong login url shows after unsuccessful login");
-        Assert.assertTrue(loginButton.isDisplayed(), "Login button is not displayed");
+        WebElement avatar = driver.findElement(By.cssSelector("[class='avatar']"));
+        Assert.assertTrue(avatar.isDisplayed(), "Avatar icon is not displayed");
+
         driver.quit();
     }
 
@@ -60,6 +65,64 @@ public class LoginTests extends BaseTest {
         passwordInput.clear();
         emailInput.sendKeys("te$t$tudent");
 
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), loginUrl, "Wrong login url shows after unsuccessful login");
+        Assert.assertTrue(loginButton.isDisplayed(), "Login button is not displayed");
+        driver.quit();
+    }
+
+    @Test (description = "Login with empty password")
+    public void LoginEmptyEmailPasswordTest() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications=*");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String loginUrl = "https://qa.koel.app/";
+        driver.get(loginUrl);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("demo@class.com");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), loginUrl, "Wrong login url shows after unsuccessful login");
+        Assert.assertTrue(loginButton.isDisplayed(), "Login button is not displayed");
+        driver.quit();
+    }
+    @Test (description = "Login with empty email and password")
+    public void LoginEmptyEmailPassword() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications=*");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String loginUrl = "https://qa.koel.app/";
+        driver.get(loginUrl);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
 
@@ -99,38 +162,4 @@ public class LoginTests extends BaseTest {
         Assert.assertTrue(loginButton.isDisplayed(), "Login button is not displayed");
         driver.quit();
     }
-
-    @Test (description = "Successful Login")
-    public void SuccessfulLogin() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications=*");
-        options.addArguments("start-maximized");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String loginUrl = "https://qa.koel.app/";
-        driver.get(loginUrl);
-
-        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("demo@class.com");
-
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("te$t$tudent");
-
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        loginButton.click();
-
-        WebElement avatar = driver.findElement(By.cssSelector("[class='avatar']"));
-        Assert.assertTrue(avatar.isDisplayed(), "Avatar icon is not displayed");
-
-        driver.quit();
-    }
-
 }
