a59966b1b4d1b243ec50c65867bfafa67896d715
dariaproit <dariad.pro.it@gmail.com>
2023-08-25 17:01:18 -0700

    coded LoginEmptyEmailPassword and Login with not existing email

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,19 +9,63 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
-    @Test
+    @Test (description = "Login with empty email and password")
     public void LoginEmptyEmailPassword() {
 
-//      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        WebElement submitLogin = driver.findElement(By.cssSelector("[type='submit']"));
+        submitLogin.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), url, "Wrong url shows after unsuccessful login");
+        Assert.assertTrue(submitLogin.isDisplayed(), "Submit button is not displayed");
         driver.quit();
     }
+
+    @Test (description = "Login with not existing email")
+    public void LoginNotExistingEmail() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("NotExistingEmail@gmail.com");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        emailInput.sendKeys("te$t$tudent");
+
+        WebElement submitLogin = driver.findElement(By.cssSelector("[type='submit']"));
+        submitLogin.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), url, "Wrong url shows after unsuccessful login");
+        Assert.assertTrue(submitLogin.isDisplayed(), "Submit button is not displayed");
+        driver.quit();
+    }
+
+
 }
